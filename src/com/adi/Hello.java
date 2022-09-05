e9e7976253ed35d05a0a517f52e48d3a0c81ea3b
AadityaSPatil <aadityapatil1810@gmail.com>
2022-09-05 19:47:46 +0530

    sample comit

---
+++ src/com/adi/Hello.java
@@ -0,0 +1,12 @@
+package com.adi;
+
+public class Hello {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Velocity");
+		System.out.println("Welcome to Testing Batch");
+		
+	}
+	}
+
